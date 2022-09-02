193918de99df9bf5b2a0ef0dde74734c861c2e88
Jorge Torres <jordeath.gedeon@gmail.com>
2022-09-01 22:47:18 -0400

    Iniciando funcion funBorrarCarpeta

---
+++ borrarCarpeta.php
@@ -0,0 +1,15 @@
+<?php 
+//BORRAR CARPETA Y SU CONTENIDO
+function funBorrarCarperta($rutaCarpeta){
+
+  	foreach(glob($rutaCarpeta . "/*") as $archivoCarpeta){             
+    	if (is_dir($archivoCarpeta)){
+      		funBorrarCarperta($archivoCarpeta);
+    	} else {
+    		unlink($archivoCarpeta);
+    	}
+  	}
+
+	rmdir($rutaCarpeta);
+}
+//-----------------------------------------------------------------------------
